72659bf6034b2bb0757748e710d35403e3dc5665
alvin0319 <alvin7810@naver.com>
2022-07-08 00:53:40 +0900

    Register recipe (WIP, need more fix)

--- src/alvin0319/SmithingTable/Loader.php
+++ src/alvin0319/SmithingTable/Loader.php
@@ -12,68 +12,92 @@ use pocketmine\block\BlockFactory;
 use pocketmine\event\EventPriority;
 use pocketmine\event\inventory\InventoryCloseEvent;
 use pocketmine\event\inventory\InventoryOpenEvent;
-use pocketmine\inventory\Inventory;
 use pocketmine\inventory\transaction\action\SlotChangeAction;
 use pocketmine\inventory\transaction\InventoryTransaction;
 use pocketmine\inventory\transaction\TransactionException;
+use pocketmine\item\Item;
+use pocketmine\item\ItemFactory;
+use pocketmine\item\VanillaItems;
+use pocketmine\network\mcpe\cache\CraftingDataCache;
 use pocketmine\network\mcpe\convert\TypeConverter;
 use pocketmine\network\mcpe\NetworkSession;
+use pocketmine\network\mcpe\protocol\CraftingDataPacket;
 use pocketmine\network\mcpe\protocol\InventoryTransactionPacket;
 use pocketmine\network\mcpe\protocol\types\inventory\ContainerIds;
+use pocketmine\network\mcpe\protocol\types\inventory\ItemStack;
 use pocketmine\network\mcpe\protocol\types\inventory\NetworkInventoryAction;
 use pocketmine\network\mcpe\protocol\types\inventory\NormalTransactionData;
+use pocketmine\network\mcpe\protocol\types\recipe\RecipeIngredient;
+use pocketmine\network\mcpe\protocol\types\recipe\ShapelessRecipe as ProtocolShapelessRecipe;
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Binary;
 use pocketmine\utils\SingletonTrait;
-
+use Ramsey\Uuid\Uuid;
+use function array_map;
 use function count;
 use function json_encode;
 
-final class Loader extends PluginBase {
+final class Loader extends PluginBase{
 	use SingletonTrait;
 
 	/** @var TransactionInventory[] */
 	private array $transactionInventory = [];
 
-	protected function onLoad(): void {
+	/** @var Item[] */
+	private array $smithingRecipes = [];
+
+	protected function onLoad() : void{
 		self::setInstance($this);
 	}
 
-	protected function onEnable(): void {
-		$this->getServer()->getPluginManager()->registerEvent(InventoryOpenEvent::class, function (InventoryOpenEvent $event) {
+	protected function onEnable() : void{
+		$this->getServer()->getPluginManager()->registerEvent(InventoryOpenEvent::class, function(InventoryOpenEvent $event){
 			$this->transactionInventory[$event->getPlayer()->getXuid()] = new TransactionInventory();
 		}, EventPriority::MONITOR, $this);
-		$this->getServer()->getPluginManager()->registerEvent(InventoryCloseEvent::class, function (InventoryCloseEvent $event) {
+		$this->getServer()->getPluginManager()->registerEvent(InventoryCloseEvent::class, function(InventoryCloseEvent $event){
 			unset($this->transactionInventory[$event->getPlayer()->getXuid()]);
 		}, EventPriority::MONITOR, $this);
 
-		SimplePacketHandler::createInterceptor($this)->interceptIncoming(function (InventoryTransactionPacket $packet, NetworkSession $session): bool {
-			if (!isset($this->transactionInventory[$session->getPlayer()->getXuid()])) return true;
+		$netheriteIngot = ItemFactory::getInstance()->get(742);
+
+		$this->registerRecipe(ItemFactory::getInstance()->get(748), VanillaItems::DIAMOND_HELMET(), $netheriteIngot);
+		$this->registerRecipe(ItemFactory::getInstance()->get(749), VanillaItems::DIAMOND_CHESTPLATE(), $netheriteIngot);
+		$this->registerRecipe(ItemFactory::getInstance()->get(750), VanillaItems::DIAMOND_LEGGINGS(), $netheriteIngot);
+		$this->registerRecipe(ItemFactory::getInstance()->get(751), VanillaItems::DIAMOND_BOOTS(), $netheriteIngot);
+		$this->registerRecipe(ItemFactory::getInstance()->get(743), VanillaItems::DIAMOND_SWORD(), $netheriteIngot);
+		$this->registerRecipe(ItemFactory::getInstance()->get(744), VanillaItems::DIAMOND_SHOVEL(), $netheriteIngot);
+		$this->registerRecipe(ItemFactory::getInstance()->get(745), VanillaItems::DIAMOND_PICKAXE(), $netheriteIngot);
+		$this->registerRecipe(ItemFactory::getInstance()->get(746), VanillaItems::DIAMOND_AXE(), $netheriteIngot);
+		$this->registerRecipe(ItemFactory::getInstance()->get(747), VanillaItems::DIAMOND_HOE(), $netheriteIngot);
+
+		SimplePacketHandler::createInterceptor($this)->interceptIncoming(function(InventoryTransactionPacket $packet, NetworkSession $session) : bool{
+			if(!isset($this->transactionInventory[$session->getPlayer()->getXuid()])) return true;
 
 			$invManager = $session->getInvManager();
 			$typeConverter = TypeConverter::getInstance();
 			$actions = [];
 			$isSmithingTableTransaction = false;
 			$isFinalTransaction = false;
-			if ($packet->trData instanceof NormalTransactionData) {
+			if($packet->trData instanceof NormalTransactionData){
 				$_actions = $packet->trData->getActions();
-				foreach ($_actions as $index => $action) {
+				foreach($_actions as $index => $action){
 					$slot = $action->inventorySlot;
-					if ($action->sourceType === NetworkInventoryAction::SOURCE_TODO && ($action->windowId === -12 || $action->windowId === -10)) {
+					if($action->sourceType === NetworkInventoryAction::SOURCE_TODO && ($action->windowId === -12 || $action->windowId === -10)){
 						$isFinalTransaction = true;
 						$inv = $this->transactionInventory[$session->getPlayer()->getXuid()];
-					} elseif ($action->sourceType === NetworkInventoryAction::SOURCE_CONTAINER) {
-						if ($action->windowId === ContainerIds::UI && ($slot === 51 || $slot === 52)) {
+					}elseif($action->sourceType === NetworkInventoryAction::SOURCE_CONTAINER){
+						if($action->windowId === ContainerIds::UI && ($slot === 51 || $slot === 52)){
 							$inv = $invManager->getWindow($invManager->getCurrentWindowId());
-							if ($inv instanceof SmithingTableInventory) {
+							if($inv instanceof SmithingTableInventory){
 								$slot -= 51;
 								$isSmithingTableTransaction = true;
-							} else {
+							}else{
 								return true; // throw error?
 							}
-						} else {
+						}else{
 							$inv = $invManager->getWindow($action->windowId);
 						}
-					} else return true;
+					}else return true;
 					$new = new NetworkInventoryAction();
 					$new->inventorySlot = $slot;
 					$new->newItem = $action->newItem;
@@ -85,46 +109,42 @@ final class Loader extends PluginBase {
 				}
 			}
 
-			$executeTransaction = function ($actions) use ($session, $invManager): bool {
+			$executeTransaction = function($actions) use ($session, $invManager) : bool{
 				$transaction = new InventoryTransaction($session->getPlayer(), $actions);
 				$invManager->onTransactionStart($transaction);
-				try {
+				try{
 					$transaction->execute();
 					return true;
-				} catch (TransactionException $e) {
+				}catch(TransactionException $e){
 					$logger = $session->getLogger();
-					$logger->debug("Failed to execute inventory transaction: ".$e->getMessage());
-					$logger->debug("Actions: ".json_encode($actions));
-
-					foreach ($transaction->getInventories() as $inventory) {
+					$logger->debug("Failed to execute inventory transaction: " . $e->getMessage());
+					$logger->debug("Actions: " . json_encode($actions));
+					foreach($transaction->getInventories() as $inventory){
 						$invManager->syncContents($inventory);
 					}
-//					var_dump($e->getMessage());
-//					var_dump($e->getTraceAsString());
 					return false;
 				}
 			};
 
-			if ($isSmithingTableTransaction && count($actions) > 0) {
-				if ($isFinalTransaction) {
+			if($isSmithingTableTransaction && count($actions) > 0){
+				if($isFinalTransaction){
 					$firstActions = []; // 기존의 제련대 템(다이아 헬멧, 네더 주괴)을 옮기는 action을 먼저 처리
 					$secondActions = []; // 제련대 결과 템(네더 헬멧)을 옮기는 action을 나중에 처리
-					foreach ($actions as $_ => $action) {
+					foreach($actions as $_ => $action){
 						$inv = $action->getInventory();
-						if ($inv instanceof SmithingTableInventory || ($inv instanceof TransactionInventory && $action->getSlot() < 2)) {
+						if($inv instanceof SmithingTableInventory || ($inv instanceof TransactionInventory && $action->getSlot() < 2)){
 							$firstActions[] = $action;
-						} else {
+						}else{
 							$secondActions[] = $action;
 						}
 					}
 					$executeTransaction($firstActions);
-//					var_dump($this->transactionInventory[$session->getPlayer()->getXuid()]->getItem(2));
-					if (count($secondActions) > 0) {
-						if ($executeTransaction($secondActions)) {
+					if(count($secondActions) > 0){
+						if($executeTransaction($secondActions)){
 							$this->transactionInventory[$session->getPlayer()->getXuid()]->clearAll(); // 네더 템 빼기 성공하면, 재료템 삭제하기
 						}
 					} // final transaction의 첫번째 패킷은 "제련대 결과 템"을 옮기는 action이 없음
-				} else {
+				}else{
 					$executeTransaction($actions);
 				}
 
@@ -134,5 +154,44 @@ final class Loader extends PluginBase {
 			return true;
 		});
 		BlockFactory::getInstance()->register(new SmithingTable(), true);
+	}
+
+	private function registerRecipe(Item $result, Item $input1, Item $input2) : void{
+		$result->setCount(1);
+		$input1->setCount(1);
+		$input2->setCount(1);
+		$packet = CraftingDataCache::getInstance()->getCache($this->getServer()->getCraftingManager());
+		static $counter = 0;
+		if($counter === 0){
+			$counter = count($packet->recipesWithTypeIds);
+		}
+		$nullUUID = Uuid::fromString(Uuid::NIL);
+		$converter = TypeConverter::getInstance();
+		$packet->recipesWithTypeIds[] = new ProtocolShapelessRecipe(
+			CraftingDataPacket::ENTRY_SHAPELESS,
+			Binary::writeInt(++$counter),
+			array_map(function(Item $item) use ($converter) : RecipeIngredient{
+				return $converter->coreItemStackToRecipeIngredient($item);
+			}, [$input1, $input2]),
+			array_map(function(Item $item) use ($converter) : ItemStack{
+				return $converter->coreItemStackToNet($item);
+			}, [$result]),
+			$nullUUID,
+			"smithing_table",
+			0,
+			$counter
+		);
+		$this->smithingRecipes[self::itemHash($input1) . self::itemHash($input2)] = clone $result;
+	}
+
+	public function getRecipe(Item $input1, Item $input2) : ?Item{
+		if(isset($this->smithingRecipes[$hash = self::itemHash($input1) . self::itemHash($input2)])){
+			return clone $this->smithingRecipes[$hash];
+		}
+		return null;
+	}
+
+	private static function itemHash(Item $item) : string{
+		return $item->getId() . ":" . $item->getMeta();
 	}
 }
--- src/alvin0319/SmithingTable/inventory/TransactionInventory.php
+++ src/alvin0319/SmithingTable/inventory/TransactionInventory.php
@@ -4,32 +4,22 @@ declare(strict_types=1);
 
 namespace alvin0319\SmithingTable\inventory;
 
-use pocketmine\block\inventory\BlockInventory;
+use alvin0319\SmithingTable\Loader;
 use pocketmine\inventory\SimpleInventory;
 use pocketmine\item\Item;
-use pocketmine\item\ItemFactory;
-use pocketmine\item\VanillaItems;
-use pocketmine\network\mcpe\protocol\ContainerClosePacket;
-use pocketmine\network\mcpe\protocol\ContainerOpenPacket;
-use pocketmine\network\mcpe\protocol\types\BlockPosition;
-use pocketmine\network\mcpe\protocol\types\inventory\WindowTypes;
-use pocketmine\player\Player;
-use pocketmine\Server;
-use pocketmine\world\Position;
 
-final class TransactionInventory extends SimpleInventory {
+final class TransactionInventory extends SimpleInventory{
 
-	public function __construct() {
+	public function __construct(){
 		parent::__construct(3);
 	}
 
-	public function getItem(int $index): Item {
-		if (
-				$index === 2 &&
-				$this->getItem(0)->equals(VanillaItems::DIAMOND_HELMET()) &&
-				$this->getItem(1)->equals(ItemFactory::getInstance()->get(742))
-		) {
-			return ItemFactory::getInstance()->get(748);
+	public function getItem(int $index) : Item{
+		if(
+			$index === 2 &&
+			($output = Loader::getInstance()->getRecipe(parent::getItem(0), parent::getItem(1))) !== null
+		){
+			$output->setNamedTag(parent::getItem(0)->getNamedTag());
 		}
 
 		return parent::getItem($index);
